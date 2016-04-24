eb3e669e659b926cdf4e381d5625f6cdd3e78d1b
Katrina Rodriguez <karodriguez8@gmail.com>
2016-04-24 12:40:30 -0400

    add third slide

--- listview2/listview2/ViewController.swift
+++ listview2/listview2/ViewController.swift
@@ -27,10 +27,14 @@ class ViewController: UIViewController, UIScrollViewDelegate {
         self.imagesScroll.addSubview(firstSlide)
         
         let secondSlide = UIView(frame: CGRect(x: 200, y: 0, width: 200, height: 200))
-        firstSlide.backgroundColor = UIColor.greenColor()
+        secondSlide.backgroundColor = UIColor.greenColor()
         self.imagesScroll.addSubview(secondSlide)
         
-        self.imagesScroll.contentSize = CGSize(width: 400, height: 0)
+        let thirdSlide = UIView(frame: CGRect(x: 400, y: 0, width: 200, height: 200))
+        thirdSlide.backgroundColor = UIColor.blueColor()
+        self.imagesScroll.addSubview(thirdSlide)
+        
+        self.imagesScroll.contentSize = CGSize(width: 600, height: 0)
         self.imagesScroll.delegate = self
         
     
@@ -44,11 +48,18 @@ class ViewController: UIViewController, UIScrollViewDelegate {
         let offset = scrollView.contentOffset.x
 //           print("scrollViewDidScroll: \(offset)")
         
-        if (offset > 100){
+        if (offset < 100){
             self.view.backgroundColor = UIColor.yellowColor()
+            self.pageControl.currentPage = 0
+
         }
-        else {
+        else if (offset < 300){
             self.view.backgroundColor = UIColor.lightGrayColor()
+            self.pageControl.currentPage = 1
+        }
+        
+        else {
+            self.pageControl.currentPage = 2
         }
     }
     
@@ -58,11 +69,15 @@ class ViewController: UIViewController, UIScrollViewDelegate {
         
         if (offset == 0){
             print("First Page")
-            self.pageControl.currentPage = 0
+//            self.pageControl.currentPage = 0
         }
         if (offset == 200){
             print("Second Page")
-            self.pageControl.currentPage = 1
+//            self.pageControl.currentPage = 1
+        }
+        if (offset == 400){
+            print("Third Page")
+            
         }
     }
     
