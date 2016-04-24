53a3ed6d40da020d6ed241e6c2af0be1b68ac569
Katrina Rodriguez <karodriguez8@gmail.com>
2016-04-24 12:12:50 -0400

    scroll view with page control

--- listview2/listview2/ViewController.swift
+++ listview2/listview2/ViewController.swift
@@ -7,18 +7,63 @@
 //
 
 import UIKit
+//conforming to the delegate protocol = add UIScrollViewDelegate to ViewController
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIScrollViewDelegate {
     
     //properties
     
     @IBOutlet var nameField: UITextField!
-    var namesArray = Array<String>()
+    @IBOutlet var imagesScroll: UIScrollView!
+    @IBOutlet var pageControl: UIPageControl!
+     var namesArray = Array<String>()
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        let firstSlide = UIView(frame: CGRect(x: 0, y: 0, width: 200, height: 200))
+        firstSlide.backgroundColor = UIColor.lightGrayColor()
+        self.imagesScroll.addSubview(firstSlide)
+        
+        let secondSlide = UIView(frame: CGRect(x: 200, y: 0, width: 200, height: 200))
+        firstSlide.backgroundColor = UIColor.greenColor()
+        self.imagesScroll.addSubview(secondSlide)
+        
+        self.imagesScroll.contentSize = CGSize(width: 400, height: 0)
+        self.imagesScroll.delegate = self
+        
+    
+    }
+    
+    //delegate callback method
+    
+    func scrollViewDidScroll(scrollView: UIScrollView) {
+     
+        
+        let offset = scrollView.contentOffset.x
+//           print("scrollViewDidScroll: \(offset)")
+        
+        if (offset > 100){
+            self.view.backgroundColor = UIColor.yellowColor()
+        }
+        else {
+            self.view.backgroundColor = UIColor.lightGrayColor()
+        }
+    }
+    
+    func scrollViewDidEndDecelerating(scrollView: UIScrollView){
+        let offset = scrollView.contentOffset.x
+        print("scrollViewDidEndDecelerating: \(offset)")
+        
+        if (offset == 0){
+            print("First Page")
+            self.pageControl.currentPage = 0
+        }
+        if (offset == 200){
+            print("Second Page")
+            self.pageControl.currentPage = 1
+        }
     }
     
     //button action
